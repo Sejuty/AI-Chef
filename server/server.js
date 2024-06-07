1551ce4e8872aad09ba0a4ed3af6e4f141aa6868
Sejuty <tafannumnishat00@gmail.com>
2024-06-07 23:58:20 +0600

    Add basic prompt

--- server/server.js
+++ server/server.js
@@ -1,13 +1,45 @@
-const express = require("express")
-const cors = require("cors")
-
+const express = require("express");
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
-const PORT = 3001
+const PORT = 3001;
+
+app.use(cors());
+require('dotenv').config();
+
+app.get("/recipeStream", (req, res) => {
+  const ingredients = req.query.ingredients;
+  const mealType = req.query.mealType;
+  const cuisine = req.query.cuisine;
+  const cookingTime = req.query.cookingTime;
+  const complexity = req.query.complexity;
+
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  const sendEvent = (chunk)=>{
+
+  }
+});
 
 
-app.use(cors())
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+async function run() {
+    const prompt = "Write a story about a magic backpack."
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+  
+  run();
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

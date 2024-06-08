6ccff8fe1a4ce68969b6f92154adaac1f2c0714a
Sejuty <tafannumnishat00@gmail.com>
2024-06-08 14:00:47 +0600

    add mistral7b as ai

--- server/server.js
+++ server/server.js
@@ -1,43 +1,44 @@
 const express = require("express");
 const cors = require("cors");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { default: ollama } = require('ollama');
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
-require('dotenv').config();
 
-app.get("/recipeStream", (req, res) => {
-  const ingredients = req.query.ingredients;
-  const mealType = req.query.mealType;
-  const cuisine = req.query.cuisine;
-  const cookingTime = req.query.cookingTime;
-  const complexity = req.query.complexity;
+// app.get("/recipeStream", (req, res) => {
+//   const ingredients = req.query.ingredients;
+//   const mealType = req.query.mealType;
+//   const cuisine = req.query.cuisine;
+//   const cookingTime = req.query.cookingTime;
+//   const complexity = req.query.complexity;
 
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
+//   res.setHeader("Content-Type", "text/event-stream");
+//   res.setHeader("Cache-Control", "no-cache");
+//   res.setHeader("Connection", "keep-alive");
 
-  const sendEvent = (chunk)=>{
+//   const sendEvent = (chunk)=>{
 
+//   }
+// });
+
+
+
+
+async function chatWithLlama() {
+  try {
+    const response = await ollama.chat({
+      model: 'mistral',
+      messages: [{ role: 'user', content: 'Create a recipe with the following instruction. Ingredients: 2 egges and potatoes, MealType: breakfast, cuisine :italian, cookingTime : % min, complexity : chef ' }],
+    });
+    console.log(response.message.content);
+  } catch (error) {
+    console.error('Error:', error);
   }
-});
+}
 
-
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-async function run() {
-    const prompt = "Write a story about a magic backpack."
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-  
-  run();
-
+chatWithLlama();
 
 
 app.listen(PORT, () => {
